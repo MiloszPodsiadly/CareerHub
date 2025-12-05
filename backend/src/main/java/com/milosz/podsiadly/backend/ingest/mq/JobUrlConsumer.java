2fd8ee68f123141b1f2e02ef1611664479d65ebf
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-12-05 19:21:36 +0100

    Setted safe 1 request per 2 second and 12h round-trip to delayString

--- backend/src/main/java/com/milosz/podsiadly/backend/ingest/mq/JobUrlConsumer.java
+++ backend/src/main/java/com/milosz/podsiadly/backend/ingest/mq/JobUrlConsumer.java
@@ -34,7 +34,7 @@ public class JobUrlConsumer {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
                     "(KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36";
 
-    private static final RateLimiter NFJ_FETCH_LIMITER = RateLimiter.create(0.3d);
+    private static final RateLimiter NFJ_FETCH_LIMITER = RateLimiter.create(0.5d);
 
     private final JustJoinParser justJoinParser;
     private final OfferUpsertService upsertService;
