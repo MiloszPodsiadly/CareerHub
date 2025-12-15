2d975d45c1db780f27a99180fb92ae9526312669
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-12-15 11:30:15 +0100

    Chnaged FavoriteController to have fallback

--- backend/src/main/java/com/milosz/podsiadly/backend/domain/loginandregister/dto/UserDto.java
+++ backend/src/main/java/com/milosz/podsiadly/backend/domain/loginandregister/dto/UserDto.java
@@ -2,4 +2,4 @@ package com.milosz.podsiadly.backend.domain.loginandregister.dto;
 
 import java.util.Set;
 
-public record UserDto(String id, String username, Set<String> roles) { }
+public record UserDto(String id, String email, Set<String> roles) { }
