b73b4814fbe8767a45d54be392548ca28159b606
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-12-17 12:46:51 +0100

    Added new method sendEmailVerification() to interface

--- backend/src/main/java/com/milosz/podsiadly/backend/domain/loginandregister/MailService.java
+++ backend/src/main/java/com/milosz/podsiadly/backend/domain/loginandregister/MailService.java
@@ -2,4 +2,5 @@ package com.milosz.podsiadly.backend.domain.loginandregister;
 
 public interface MailService {
     void sendPasswordResetEmail(String to, String resetLink);
+    void sendEmailVerification(String to, String verifyLink);
 }
