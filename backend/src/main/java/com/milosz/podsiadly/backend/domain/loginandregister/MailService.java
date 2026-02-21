b42108462e67919a76a917d3944190a76c425acf
Milosz Podsiadly <m.podsiadly99@gmail.com>
2026-02-21 19:11:17 +0100

    refactor(auth): remove SMTP MailService

--- backend/src/main/java/com/milosz/podsiadly/backend/domain/loginandregister/MailService.java
+++
@@ -1,6 +0,0 @@
-package com.milosz.podsiadly.backend.domain.loginandregister;
-
-public interface MailService {
-    void sendPasswordResetEmail(String to, String resetLink);
-    void sendEmailVerification(String to, String verifyLink);
-}
